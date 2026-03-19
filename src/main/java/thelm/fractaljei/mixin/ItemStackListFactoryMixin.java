4a14563967272a0719c73ae4e0ab1a3cc947e6a5
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2026-03-20 01:59:56 +0800

    Modified mixin for the case where tabs add items differently in different tabs

--- src/main/java/thelm/fractaljei/mixin/ItemStackListFactoryMixin.java
+++ src/main/java/thelm/fractaljei/mixin/ItemStackListFactoryMixin.java
@@ -1,17 +1,18 @@
 package thelm.fractaljei.mixin;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
-import de.dafuqs.fractal.api.CreativeSubTab;
+import de.dafuqs.fractal.interfaces.ICreativeTabParent;
 import mezz.jei.library.plugins.vanilla.ingredients.ItemStackListFactory;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -22,20 +23,39 @@ public class ItemStackListFactoryMixin {
 	@Shadow
 	private static Logger LOGGER;
 
-	@Inject(method = "create", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/CreativeModeTabs;allTabs()Ljava/util/List;"))
-	private static void fractaljei$buildSubTabContents(CallbackInfoReturnable<List<ItemStack>> info, @Local CreativeModeTab.ItemDisplayParameters displayParameters) {
-		for(CreativeModeTab tab : CreativeSubTab.SUBTABS) {
-			if(tab.getType() != CreativeModeTab.Type.CATEGORY) {
-				LOGGER.debug("Skipping creative tab: '{}' because it is type: {}", tab.getDisplayName().getString(), tab.getType());
-				continue;
+	@WrapOperation(method = "create", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/CreativeModeTab;buildContents(Lnet/minecraft/world/item/CreativeModeTab$ItemDisplayParameters;)V"))
+	private static void fractaljei$buildSubTabContents(CreativeModeTab tab, CreativeModeTab.ItemDisplayParameters displayParameters, Operation<Void> original) {
+		if(tab instanceof ICreativeTabParent parent && !parent.fractal$getChildren().isEmpty()) {
+			for(CreativeModeTab subTab : parent.fractal$getChildren()) {
+				try {
+					subTab.buildContents(displayParameters);
+				}
+				catch(Throwable e) {
+					LOGGER.error("Item subgroup crashed while building contents. Items from this subgroup will be missing from the JEI ingredient list: {}", subTab.getDisplayName().getString(), e);
+				}
 			}
-			try {
-				tab.buildContents(displayParameters);
+		}
+		else {
+			original.call(tab, displayParameters);
+		}
+	}
+
+	@WrapOperation(method = "create", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/CreativeModeTab;getDisplayItems()Ljava/util/Collection;"))
+	private static Collection<ItemStack> fractaljei$getSubTabContents(CreativeModeTab tab, Operation<Collection<ItemStack>> original) {
+		if(tab instanceof ICreativeTabParent parent && !parent.fractal$getChildren().isEmpty()) {
+			List<ItemStack> stacks = new ArrayList<>();
+			for(CreativeModeTab subTab : parent.fractal$getChildren()) {
+				try {
+					stacks.addAll(subTab.getDisplayItems());
+				}
+				catch(Throwable e) {
+					LOGGER.error("Item subgroup crashed while getting tab display items. Items from this subgroup will be missing from the JEI ingredient list: {}", subTab.getDisplayName().getString(), e);
+				}
 			}
-			catch(Throwable e) {
-				LOGGER.error("Item Group crashed while building contents. Items from this group will be missing from the JEI ingredient list: {}", tab.getDisplayName().getString(), e);
-				continue;
-			}
+			return stacks;
+		}
+		else {
+			return original.call(tab);
 		}
 	}
 }
